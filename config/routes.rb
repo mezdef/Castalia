290b5c390370da4776b09b5f4bb159ea6b2eb7fe
Marc Ziani de Ferranti <mezdef@gmail.com>
2013-07-27 19:42:47 +1000

    Adding 'About' static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Castalia::Application.routes.draw do
+  get "about/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
