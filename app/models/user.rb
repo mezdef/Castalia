78a71331cfc93bf5201b870d2e2317bb5c8f4d1e
Marc Ziani de Ferranti <mezdef@gmail.com>
2013-11-22 09:01:14 +1100

    Adding signup status to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ActiveRecord::Base
-  rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  rolify
+  # resourcify
+
+  def active_for_authentication? 
+    super && status == "approved"
+  end 
+
+  def inactive_message 
+    if status? == "pending"
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
+
+
 end
