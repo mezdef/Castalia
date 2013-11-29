99b5ab546a201716d915a90c8b459ca52300b2db
Marc Ziani de Ferranti <mezdef@gmail.com>
2013-11-29 16:38:00 +1100

    Fix: Registration procedure and default roles for registrants and new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user! , :except => [:new, :resetpassword]
+  before_filter :authenticate_user!, :except => [:new, :resetpassword]
   load_and_authorize_resource
+  skip_authorize_resource :only => :new
+
 
   def index
     @approveduser = User.find_all_by_status("approved")
@@ -17,10 +19,15 @@ class UsersController < ApplicationController
   #   # redirect_to root_url
   end
 
+  def new
+  end
+
   def create
     @user = User.new(params[:user])
+    @user.add_role(:user)
 
     if @user.save
+      # UserMailer.account_requested(@user).deliver
       redirect_to @user, :flash => { :success => 'User was successfully created.' }
     else
       render :action => 'new'
@@ -34,9 +41,8 @@ class UsersController < ApplicationController
     params[:user].delete(:password) if params[:user][:password].blank?
     params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
 
-    # params[:user][:role_ids] ||= []
+    params[:user][:role_ids] ||= []
 
-    # if @user.update_attributes(params.require(:user).permit(:first_name, :last_name, :status, :role, :role_ids, :password, :password_confirmation))
     if @user.update_attributes(user_params)
       # redirect_to user_path
       sign_in(@user, :bypass => true) if @user == current_user
@@ -50,14 +56,15 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_path, :flash => { :success => 'User was successfully deleted.' }
+    redirect_to users_path
+    flash[:notice] = "Successfully deleted #{@user.email}."
   end
 
   private
   def user_params
     if can? :manage, User
       # params[:user]
-      params.require(:user).permit(:email, :first_name, :last_name, :contact, :status, :role, {:role_ids => []}, :password, :password_confirmation, )
+      params.require(:user).permit(:email, :first_name, :last_name, :contact, :status, :role, {:role_ids => []}, :password, :password_confirmation )
     else
       params[:user].permit(:first_name, :last_name, :password, :password_confirmation)
     end
