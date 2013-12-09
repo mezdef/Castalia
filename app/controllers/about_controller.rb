78be4c584da1b825318e2dfd87f8817bb4835555
Marc Ziani de Ferranti <mezdef@gmail.com>
2013-12-09 16:03:49 +1100

    Feature - About: Allowing editing of multiple records with mercury

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -6,10 +6,13 @@ class AboutController < ApplicationController
   
   def mercury_update
     # Update page
-    about = Text.first
-    about.title = params[:content][:about_title][:value]
+    @about = Text.all
+    @about.each do |t|
+      puts(t.id)
+      t.title = params[:content]["about_title_" + "#{t.id}"][:value]
+      t.save!
+    end
     # about.content = params[:content][:value]
-    about.save!
     render text: ""
   end
 
