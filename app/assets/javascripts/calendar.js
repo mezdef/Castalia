61e08fe0ea03d92734ebddb21730be7acca93bf1
Marc Ziani de Ferranti <mezdef@gmail.com>
2014-02-24 22:01:11 +1100

    Update - Calendar: JS and CSS magic to extend td and colour events

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
-  $('li[event-id].start:not([stacked])').each(function() {
 
-    var evNum = $(this).attr('event-id');
-    var evLev;
-    var evLevA = [];
+  var evWeekLev = [];
+  var i = 0;
+  $('tr.week').each(function() {
+    $(this).attr('week', i);
+    $(this).children().children().children('li[event-id].start:not([stacked])').each(function() {
 
-    $('li[event-id=' + '"' + evNum + '"]:not([stacked])').each(function(){
-      var evLevN = $(this).attr('stack-level');
-      evLevA.push(parseInt(evLevN));
+      var evNum = $(this).attr('event-id');
+      var evLev;
+      var evLevA = [];
+
+      $('li[event-id=' + '"' + evNum + '"]:not([stacked])').each(function(){
+        var evLevN = $(this).attr('stack-level');
+        evLevA.push(parseInt(evLevN));
+      });
+
+      evLev = Math.max.apply(Math, evLevA);
+      $('li[event-id=' + '"' + evNum + '"]:not([stacked])').addClass('ev_' + evLev).attr('stacked', '');
+      evWeekLev.push(evLev);
+      evLevA = [];
     });
-
-    evLev = Math.max.apply(Math, evLevA);
-    $('li[event-id=' + '"' + evNum + '"]:not([stacked])').addClass('ev_' + evLev).attr('stacked', '');
-    
-    evLevA = [];
+    if (evWeekLev < 1) {
+    } else {
+      var zz = Math.max.apply(Math, evWeekLev) +1;
+      $('tr[week="' + i +'"').addClass('events' +zz);
+    }
+    i++;
+    evWeekLev = [];
   });
+
 });
--- lib/calendar.rb
+++ lib/calendar.rb
@@ -18,7 +18,7 @@ class Calendar < Struct.new(:view, :date, :callback)
  
     def week_rows
       weeks.map do |week|
-        content_tag :tr do
+        content_tag :tr, class: 'week' do
           week.map { |day| day_cell(day) }.join.html_safe
         end
       end.join.html_safe
